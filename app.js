7eeee02c5d40c51a0c16eb293fcb38564b09c592
StoneT2000 <stoneztao@gmail.com>
2019-10-18 12:06:08 -0700

    All

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/test', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/web/public/index.html'));
+});
