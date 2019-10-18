f7fbcd4afb9bb463859db34ae7883ff74b05038f
StoneT2000 <stoneztao@gmail.com>
2019-10-18 15:16:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 const app = express();
@@ -22,3 +22,36 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+*/
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'web/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'web/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
