7b860afc8a4395705e36148e259304fc17b122ff
StoneT2000 <stoneztao@gmail.com>
2019-10-18 15:11:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ app.get('/test', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
 //app.use(express.static(path.join(__dirname, 'web/public')));
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
